61051b5f100866620459965d3f100baa66b1b13b
Emir Maya <emirmayaok@gmail.com>
2026-01-21 20:27:26 -0300

    details

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
+    <main className="min-h-screen flex items-center justify-center bg-amber-200">
       <h1 className="text-4xl font-bold">
         Bitcoin Rainbow <span className="text-blue-500">MVP</span>
+Aquí se presentarán charts de bitcoin
       </h1>
     </main>
   );
